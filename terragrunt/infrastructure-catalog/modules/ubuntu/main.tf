72dd1c3fb6d101d8161881f58e2647851f20e177
roib20 <66280613+roib20@users.noreply.github.com>
2025-05-20 01:59:47 +0300

    Update Ubuntu Terragrunt files
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/ubuntu/main.tf
+++ terragrunt/infrastructure-catalog/modules/ubuntu/main.tf
@@ -1,35 +1,89 @@
-resource "proxmox_virtual_environment_vm" "ubuntu" {
-  name      = "ubuntu"
-  node_name = "pve"
-  vm_id     = 3001
+########################################################
+# Resources
+########################################################
 
-  template = false
-  started  = true
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type       = "iso"
+  datastore_id       = var.iso_datastore_id
+  node_name          = var.node_name
+
+  url                = var.iso_url
+  checksum           = var.iso_checksum
+  checksum_algorithm = var.iso_checksum_algorithm
+}
+
+data "local_file" "ssh_public_key" {
+  filename = "./id_rsa.pub"
+}
+
+resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  name      = var.vm_name
+  node_name = var.node_name
+  vm_id     = var.vm_id
+
+  template  = false
+  started   = true
+
+  machine     = "q35"
+  bios        = "ovmf"
+  description = "Managed by Terragrunt"
+
+  initialization {
+      datastore_id = var.vm_datastore_id
+      ip_config {
+        ipv4 {
+          address = "${var.ipv4_address}/24"
+          gateway = var.ipv4_gateway
+        }
+      }
+      user_account {
+        username = var.username
+        keys     = [trimspace(data.local_file.ssh_public_key.content)]
+      }
+  }
+
+  agent {
+    enabled = false
+  }
 
   operating_system {
     type = "l26"
   }
 
   cpu {
-    type   = "host"
-    cores  = 4
-    sockets = 1
+    type = "host"
   }
 
   memory {
-    dedicated = 8192
+    dedicated = var.memory_dedicated
   }
 
   scsi_hardware = "virtio-scsi-single"
 
+  ##### Primary boot disk (ISO) #####
   disk {
-    datastore_id = "local-lvm"
+    datastore_id = var.vm_datastore_id
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
     interface    = "virtio0"
-    size         = 32
     iothread     = true
+    discard      = "on"
+    size         = var.disk_size_gb
   }
 
+  ##### EFI disk #####
+  efi_disk {
+    datastore_id      = var.vm_datastore_id
+    type              = "4m"
+    pre_enrolled_keys = true
+  }
+
+  ##### CD-ROM #####
+  cdrom {
+    interface = "ide0"
+  }
+
+  ##### Network device #####
   network_device {
-    bridge = "vmbr0"
+    bridge = var.bridge
   }
 }
--- terragrunt/infrastructure-live/_envcommon/ubuntu.hcl
+++ terragrunt/infrastructure-live/_envcommon/ubuntu.hcl
@@ -4,4 +4,24 @@
 # environments.
 # ---------------------------------------------------------------------------------------------------------------------
 inputs = {
+    # Proxmox target
+  node_name = "pve"
+
+  # ISO download
+  iso_url                = "https://cloud-images.ubuntu.com/releases/noble/release-20250516/ubuntu-24.04-server-cloudimg-amd64.img"
+  iso_checksum           = "8d6161defd323d24d66f85dda40e64e2b9021aefa4ca879dcbc4ec775ad1bbc5"
+  iso_checksum_algorithm = "sha256"
+  iso_datastore_id       = "local"
+
+  # VM identity
+  vm_name = ""          # leave empty or set e.g. "truenas"
+  vm_id   = 4000        # override as needed
+
+  # Storage & resources
+  vm_datastore_id  = "VM"
+  memory_dedicated = 4096
+  disk_size_gb     = 32
+
+  # IPv4
+  ipv4_address = "192.168.1.20"
 }
