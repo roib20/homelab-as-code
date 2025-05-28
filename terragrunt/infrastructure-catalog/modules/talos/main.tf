7e12bf5f03eeadf6ba167dc56600c1c163d4e7ea
roib20 <66280613+roib20@users.noreply.github.com>
2025-05-28 23:15:33 +0300

    Fix wrong cluster_endpoint port
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/talos/main.tf
+++ terragrunt/infrastructure-catalog/modules/talos/main.tf
@@ -2,14 +2,14 @@ resource "talos_machine_secrets" "this" {}
 
 data "talos_machine_configuration" "controlplane" {
   cluster_name     = var.cluster_name
-  cluster_endpoint = "https://${var.cluster_endpoint}:8006"
+  cluster_endpoint = "https://${var.cluster_endpoint}:6443"
   machine_type     = "controlplane"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
 
 data "talos_machine_configuration" "worker" {
   cluster_name     = var.cluster_name
-  cluster_endpoint = "https://${var.cluster_endpoint}:8006"
+  cluster_endpoint = "https://${var.cluster_endpoint}:6443"
   machine_type     = "worker"
   machine_secrets  = talos_machine_secrets.this.machine_secrets
 }
