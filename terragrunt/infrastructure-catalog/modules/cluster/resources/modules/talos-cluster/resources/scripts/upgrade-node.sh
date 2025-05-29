c3f429b2506e053a66d452b25e7d7a7b22c1e0bd
roib20 <66280613+roib20@users.noreply.github.com>
2025-05-29 22:08:13 +0300

    Safely quote shell variables
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/cluster/resources/modules/talos-cluster/resources/scripts/upgrade-node.sh
+++ terragrunt/infrastructure-catalog/modules/cluster/resources/modules/talos-cluster/resources/scripts/upgrade-node.sh
@@ -12,20 +12,20 @@ if [ -z "$DESIRED_TALOS_TAG" ] || [ -z "$DESIRED_TALOS_SCHEMATIC" ] || [ -z "$TA
   exit 1
 fi
 
-echo "config: $TALOS_CONFIG_PATH"
-echo "Upgrade check running on: $TALOS_NODE"
+echo "config: \"$TALOS_CONFIG_PATH\""
+echo "Upgrade check running on: \"$TALOS_NODE\""
 echo "Waiting for this node to be available..."
 for i in {1..12}; do talosctl --talosconfig "$TALOS_CONFIG_PATH" get machinestatus --nodes "$TALOS_NODE" -o json 2>/dev/null | jq -e '.spec.status.ready' >/dev/null && break || sleep 10; done
 echo "✅ Node is reporting ready!"
 
 #echo "Waiting for this cluster to be healthy..."
-#for i in {1..12}; do talosctl --talosconfig $TALOS_CONFIG_PATH health --nodes "$TALOS_NODE" && break || sleep 10; done
+#for i in {1..12}; do talosctl --talosconfig "$TALOS_CONFIG_PATH" health --nodes "$TALOS_NODE" && break || sleep 10; done
 
 CURRENT_TALOS_SCHEMATIC=$(talosctl --talosconfig "$TALOS_CONFIG_PATH" --nodes "$TALOS_NODE" get extensions -o json 2>/dev/null | jq -s '.[] | select(.spec.metadata.name == "schematic") | .spec.metadata.version' | tr -d '"')
 CURRENT_TALOS_TAG=$(talosctl --talosconfig "$TALOS_CONFIG_PATH" --nodes "$TALOS_NODE" read /etc/os-release 2>/dev/null | awk -F= '/^VERSION_ID=/ {print $2}')
 
-echo Current Schematic: $CURRENT_TALOS_SCHEMATIC Desired Schematic: $DESIRED_TALOS_SCHEMATIC
-echo Current Tag: $CURRENT_TALOS_TAG Desired Tag: $DESIRED_TALOS_TAG
+echo "Current Schematic: \"$CURRENT_TALOS_SCHEMATIC\" Desired Schematic: \"$DESIRED_TALOS_SCHEMATIC\""
+echo "Current Tag: \"$CURRENT_TALOS_TAG\" Desired Tag: \"$DESIRED_TALOS_TAG\""
 
 if [ "$DESIRED_TALOS_TAG" = "$CURRENT_TALOS_TAG" ] && [ "$DESIRED_TALOS_SCHEMATIC" = "$CURRENT_TALOS_SCHEMATIC" ]; then
   echo "No Upgrade required."
