69eccc568f881fe76d17676fb333f518d170be7f
roib20 <66280613+roib20@users.noreply.github.com>
2025-05-29 17:33:42 +0300

    Remove previous `talos` module
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/talos/nodes.tf
+++
@@ -1,5 +0,0 @@
-locals {
-    controlplane_nodes = [for k, v in var.node_data.controlplanes : k]
-    worker_nodes       = [for k, v in var.node_data.workers : k]
-    all_nodes          = concat(local.controlplane_nodes, local.worker_nodes)
-}
