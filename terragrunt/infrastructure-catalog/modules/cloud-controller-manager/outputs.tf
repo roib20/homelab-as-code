89a7a8785bc81034a5e41060249eb36ba476efa1
roib20 <66280613+roib20@users.noreply.github.com>
2025-06-04 21:39:49 +0300

    Disable irrelevant resources for `cloud-controller-manager` module
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/cloud-controller-manager/outputs.tf
+++ terragrunt/infrastructure-catalog/modules/cloud-controller-manager/outputs.tf
@@ -1,18 +1,18 @@
-output "user_data_cloud_config" {
-  value       = "${var.datastore_id}:${var.content_type}/user-data-cloud-config.yaml"
-}
+# output "user_data_cloud_config" {
+#   value       = "${var.datastore_id}:${var.content_type}/user-data-cloud-config.yaml"
+# }
 
-output "meta_data_cloud_config" {
-  value       = "${var.datastore_id}:${var.content_type}/meta-data-cloud-config.yaml"
-}
+# output "meta_data_cloud_config" {
+#   value       = "${var.datastore_id}:${var.content_type}/meta-data-cloud-config.yaml"
+# }
 
-output "vendor_data_cloud_config" {
-  value       = "${var.datastore_id}:${var.content_type}/vendor-data-cloud-config.yaml"
-}
+# output "vendor_data_cloud_config" {
+#   value       = "${var.datastore_id}:${var.content_type}/vendor-data-cloud-config.yaml"
+# }
 
-output "user_data_file_id" {
-  value = proxmox_virtual_environment_file.user_data_cloud_config.id
-}
+# output "user_data_file_id" {
+#   value = proxmox_virtual_environment_file.user_data_cloud_config.id
+# }
 
 output "meta_data_file_id" {
   value = proxmox_virtual_environment_file.meta_data_cloud_config.id
