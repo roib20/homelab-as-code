8b58f64712c5d9e1146abd1ed73111c1689ae541
roib20 <66280613+roib20@users.noreply.github.com>
2025-06-04 15:41:29 +0300

    Update cloud-config to support meta-data
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/cloud-config/outputs.tf
+++ terragrunt/infrastructure-catalog/modules/cloud-config/outputs.tf
@@ -9,3 +9,15 @@ output "meta_data_cloud_config" {
 output "vendor_data_cloud_config" {
   value       = "${var.datastore_id}:${var.content_type}/vendor-data-cloud-config.yaml"
 }
+
+output "user_data_file_id" {
+  value = proxmox_virtual_environment_file.user_data_cloud_config.id
+}
+
+output "meta_data_file_id" {
+  value = proxmox_virtual_environment_file.meta_data_cloud_config.id
+}
+
+# output "vendor_data_file_id" {
+#   value = proxmox_virtual_environment_file.vendor_data_cloud_config.id
+# }
