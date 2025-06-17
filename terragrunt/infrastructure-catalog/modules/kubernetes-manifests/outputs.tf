77470f68f2d17629dc215e2a063db1d1c6f714e6
roib20 <66280613+roib20@users.noreply.github.com>
2025-06-17 04:07:38 +0300

    Fix `kubernetes-manifests` module
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/kubernetes-manifests/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_name" {
-  description = "The name of the applied Kubernetes resource"
-  value       = try(kubernetes_manifest.apply.manifest["metadata"]["name"], null)
-}
-
-output "resource_kind" {
-  description = "The kind of the applied Kubernetes resource"
-  value       = try(kubernetes_manifest.apply.manifest["kind"], null)
-}
