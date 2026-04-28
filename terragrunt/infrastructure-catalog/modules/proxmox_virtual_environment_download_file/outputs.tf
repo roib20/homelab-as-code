3a81a4395f860a12f5574cbe48e0b7e56b3d81c2
roib20 <66280613+roib20@users.noreply.github.com>
2026-04-29 01:56:25 +0300

    fix(terragrunt): replace deprecated `proxmox_virtual_environment_download_file` with `proxmox_download_file`
    The `proxmox_virtual_environment_download_file` resource is deprecated
    and will be removed in provider v1.0. Rename to `proxmox_download_file`
    in the module's `main.tf` and `outputs.tf`.
    
    Signed-off-by: roib20 <66280613+roib20@users.noreply.github.com>

--- terragrunt/infrastructure-catalog/modules/proxmox_virtual_environment_download_file/outputs.tf
+++ terragrunt/infrastructure-catalog/modules/proxmox_virtual_environment_download_file/outputs.tf
@@ -1,17 +1,17 @@
 output "downloaded_file_id" {
   description = "(String) The unique identifier of the downloaded file resource."
-  value       = values(proxmox_virtual_environment_download_file.download)[0].id
+  value       = values(proxmox_download_file.download)[0].id
 }
 
 output "downloaded_file_size" {
   description = "(Number) The file size."
-  value       = values(proxmox_virtual_environment_download_file.download)[0].size
+  value       = values(proxmox_download_file.download)[0].size
 }
 
 output "downloaded_file_ids" {
   description = "(Map) IDs of downloaded files, keyed by each node/image combination"
   value = {
-    for resource_key, file in proxmox_virtual_environment_download_file.download :
+    for resource_key, file in proxmox_download_file.download :
     resource_key => file.id
   }
 }
@@ -19,7 +19,7 @@ output "downloaded_file_ids" {
 output "downloaded_file_sizes" {
   description = "(Map) Sizes of downloaded files, keyed by each node/image combination"
   value = {
-    for resource_key, file in proxmox_virtual_environment_download_file.download :
+    for resource_key, file in proxmox_download_file.download :
     resource_key => file.size
   }
 }
